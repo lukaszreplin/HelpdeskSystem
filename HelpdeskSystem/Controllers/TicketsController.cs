e93d9df1868d91a5ffaa4e7ae4db0ce648b64608
lukaszek016 <lukaszek016@gmail.com>
2018-06-17 01:40:15 +0200

    Add editing tickets

--- HelpdeskSystem/Controllers/TicketsController.cs
+++ HelpdeskSystem/Controllers/TicketsController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
-using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -76,7 +75,6 @@ namespace HelpdeskSystem.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Ticket ticket = db.Tickets.Find(id);
-            //ticket.ModifiedDate = DateTime.Now;
             if (ticket == null)
             {
                 return HttpNotFound();
@@ -91,14 +89,12 @@ namespace HelpdeskSystem.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Subject,Content,StatusId")] Ticket ticket)
+        public ActionResult Edit([Bind(Include = "Id,Subject,Content,CreatedDate,ModifiedDate,StatusId,ProfileId")] Ticket ticket)
         {
             if (ModelState.IsValid)
             {
-
-                //db.Entry(ticket).State = EntityState.Modified;
-                //db.Tickets.Where(t => t.Id = ticket.Id).Update;
-                //ticket.ModifiedDate = DateTime.Now;
+                ticket.ModifiedDate = DateTime.Now;
+                db.Entry(ticket).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
